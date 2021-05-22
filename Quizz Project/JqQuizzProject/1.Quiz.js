2f1b16bdf7e16c0341ab0b9c7dde27a6afb10ee7
Soe Zeya Paing <69259187+MgPaingPaing@users.noreply.github.com>
2021-05-23 01:04:27 +0630

    Quizz Project (Jquery)

---
+++ Quizz Project/JqQuizzProject/1.Quiz.js
@@ -0,0 +1,35 @@
+function submitAnswers() {
+    let total = 5;
+    let score = 0;
+
+    //Get User Input
+    let q1 = document.forms["quizForm"]["q1"].value;
+    let q2 = document.forms["quizForm"]["q2"].value;
+    let q3 = document.forms["quizForm"]["q3"].value;
+    let q4 = document.forms["quizForm"]["q4"].value;
+    let q5 = document.forms["quizForm"]["q5"].value;
+
+    //validation
+    for(i = 1 ; i <= total ; i++){
+        if(eval("q"+i)==  null || eval("q"+i) ==''){
+            alert("You missed question" + i)
+            return false;
+        }
+    } 
+     //Set Correct Answers
+      let answers = ["b","a","d","b","d"];
+
+      //Check Answers
+      for(i = 1 ; i <= total ; i++){
+        if(eval("q"+i) == answers[i - 1 ] ){
+           score++;
+           
+        }
+    } 
+    //Display Results
+    let results =document.getElementById('results')
+    results.innerHTML ='<h1>You scored <b>' +score+'</b> out of <b>'+total+'</b></h1>'
+    alert('You scored '+score+'out of' +total)
+    
+    return false;
+}
